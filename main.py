6eb0a248da100b83fc50891a2c389d9bc8bcbfa1
Yipeng Jiang <yipeng.jiang@DEWILMYJIANGM.local>
2023-04-02 13:02:50 -0500

    fix the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from io import BytesIO
 import pandas as pd
 
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
 def split_item(s):
@@ -13,36 +22,64 @@ def split_material(s):
         return pd.Series([s[0], s[1]])
     else:
         return pd.Series([s[0], '无'])
+    
+def split_location(s):
+    if '棚' in s:
+        return pd.Series('顶棚')
+    elif '地' in s:
+        return pd.Series('地面')
+    elif '墙' in s:
+        return pd.Series('墙面')
+    else:
+        return pd.Series('其他')
+    
+    
+    
+    
 
-
-# Layout
-
-if 'uploaded_file' not in st.session_state:
-    st.session_state['uploaded_file'] =  None
- 
-  
-main = st.empty()
-
-if st.session_state['uploaded_file'] is None:
-    with main.container():
-        st.session_state['uploaded_file'] = st.file_uploader("请选择文件上传")
-        st.button('上传')
-else:
-    with main.container():
-        st.dataframe(data.head(),width=1200)
-
-
-
-# Using "with" notation
+# sidebar------------------------------------
 with st.sidebar:
     st.title('工程项目汇总')
-    
-    add_selectbox = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
+  
+    file_binary =  st.file_uploader("请选择工程文件上传", type=['xlsx']) 
+    st.session_state['uploaded_file'] = file_binary
+    category = st.radio(
+        "选择汇总分类",
+        ('位置','项目总称','具体材料')
     )
+    # group_action = st.button("开始汇总", type="primary")
+    
 
+# Main content------------------------------------
+
+raw_tab, processed_tab = st.tabs(["原始表格", "分类汇总"])
+with raw_tab:
+    if file_binary:
+        try:
+            data =  pd.read_excel(file_binary,index_col=0)
+            data.columns = data.iloc[2,:]
+            data = data[pd.to_numeric(data.index, errors='coerce').notnull()][['项目编码','项目名称\n项目特征','计量单位','工程数量' ]]
+            data = data.rename(columns = {'项目名称\n项目特征':'项目特征'})
+            st.dataframe(data,use_container_width=True, height=450)
+        except:
+            st.warning('文件格式不符，请检查后刷新页面重新上传', icon="⚠️")
+
+with processed_tab:
+    if file_binary:
+        processed = data.copy()
+        processed['项目'] = processed['项目特征'].apply(split_item)
+        processed[['项目总称','具体材料']] = processed['项目'].apply(split_material)
+        processed['位置'] = processed['项目总称'].apply(split_location)
+        result = processed.groupby([category])['工程数量'].agg('sum').to_frame()
+        st.dataframe(result,height=450,use_container_width=True)
+        
+        all_result =processed.groupby(['位置','项目总称','具体材料','项目特征'])['工程数量'].agg('sum').to_frame()
+        
+        output = BytesIO()
+        all_result.to_excel(output)
+        st.download_button(
+            label="下载汇总数据",
+            data=output,
+            file_name='汇总.xlsx',
+            
+        )
